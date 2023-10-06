02db23488d58f05efe1e0f4245878f85d47c5f0a
dingogap <petermedbury@Betelgeuse.local>
2023-10-06 20:12:50 +1100

    replaced Magic Strings in dtabase.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,13 +1,15 @@
 import { openDB } from 'idb';
 
+const JATE=JATE
+
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB(JATE, 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains(JATE)) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore(JATE, { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
@@ -15,9 +17,9 @@ const initdb = async () =>
 // Add all data to a single record, nominally called '1'
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const jateDb = await openDB(JATE, 1);
+  const tx = jateDb.transaction(JATE, 'readwrite');
+  const store = tx.objectStore(JATE);
   const request = store.put({id: "1" , value: content });
   const result = await request;
   console.log('Data saved to the database', result);
@@ -26,9 +28,9 @@ export const putDb = async (content) => {
 // Get all data from a single record, nominally called '1'
 export const getDb = async () => {
   console.log('GET from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  const jateDb = await openDB(JATE, 1);
+  const tx = jateDb.transaction(JATE, 'readonly');
+  const store = tx.objectStore(JATE);
   const request = store.get("1");
   const result = await request;
   console.log('result.value', result.value);
